b4bdaffe60ce40430025ef6aad01770389648bc4
morteza-eskandarian <morteza.eskandarian@gmail.com>
2025-06-06 09:56:56 -0300

    updated baseURL

--- lib/utils/server_configuration.dart
+++ lib/utils/server_configuration.dart
@@ -11,7 +11,7 @@
 // Application Specific Constants
 const int defaultSplashDelay = 3;
 
-const String baseUrl = "";
+const String baseUrl = "http://ec2-13-40-131-17.eu-west-2.compute.amazonaws.com/graphql";
 
 ///default channel id
 const String defaultChannelId = "1";
